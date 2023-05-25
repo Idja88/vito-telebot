7e0eef6e006ad31bf6fb031121937e1ec38a010d
Idja88 <idja08@gmail.com>
2023-05-25 15:50:48 +0600

    Added Notification, ChatData, Fixes and Tweaks

--- bot/bot.py
+++ bot/bot.py
@@ -26,8 +26,6 @@ btn2 = types.KeyboardButton('Отписаться')
 KeyBoard.row(btn1, btn2)
 
 def botactions():
-    #Set all your bot handlers inside this function
-    #If bot is used as a global variable, remove bot as an input param
     @bot.message_handler(commands=['start'])
     def start_command(message):
         bot.send_message(
@@ -49,8 +47,8 @@ def botactions():
         phone = str(message.contact.phone_number)
         phone = re.sub('[^A-Za-z0-9]+', '', phone)
         try:
-            if not sp.check_phone(chat):
-                sp.new_subscriber(phone,chat)
+            if not sp.check_subscriber(chat):
+                sp.add_subscriber(phone, chat)
                 bot.reply_to(message, 'Спасибо за подписку')
             else:
                 bot.reply_to(message, 'Вы уже подписаны')
@@ -61,7 +59,7 @@ def botactions():
     def unsubscribe(message):
         chat = str(message.chat.id)
         try:
-            if not sp.check_phone(chat):
+            if not sp.check_subscriber(chat):
                 bot.reply_to(message, 'Вы ещё не подписаны')
             else:
                 sp.delete_subscriber(chat)
@@ -69,39 +67,36 @@ def botactions():
         except:
             print("Error")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    print(f"Получено новое сообщение от {message.chat.username}: {message.text}")
-    print(message)
-    #bot.reply_to(message, message.text)
-
 def bot_polling():
-    #global bot #Keep the bot object as global variable if needed
     global bot
     print("Starting bot polling now")
     while True:
         try:
             print("New bot instance started")
-            botactions() #If bot is used as a global variable, remove bot as an input param
-            bot.polling(none_stop=True, interval=BOT_INTERVAL, timeout=BOT_TIMEOUT)
-        except Exception as ex: #Error in polling
+            botactions()
+            bot.infinity_polling()
+        except Exception as ex:
             print("Bot polling failed, restarting in {}sec. Error:\n{}".format(BOT_TIMEOUT, ex))
             bot.stop_polling()
             sleep(BOT_TIMEOUT)
-        else: #Clean exit
+        else:
             bot.stop_polling()
             print("Bot polling loop finished")
-            break #End loop
+            break
 
 polling_thread = threading.Thread(target=bot_polling)
 polling_thread.daemon = True
 polling_thread.start()
 
-#Keep main program running while bot runs threaded
+chat_data = {}
+
 if __name__ == "__main__":
     while True:
         try:
-            sp.get_changes()
-            #sleep(120)
+            if sp.is_assignedto_subscriber(sp.get_changes(), chat_data) is True:
+                chat_id = chat_data['TeleChat']
+                task_id = chat_data['TaskId']
+                bot.send_message(chat_id, text = f"новая задача {task_id}")
+                #sleep(120)
         except KeyboardInterrupt:
             break
--- bot/sharepoint.py
+++ bot/sharepoint.py
@@ -16,14 +16,13 @@ PASSWORD = spconfig['password']
 SHAREPOINT_URL = spconfig['url']
 SHAREPOINT_DESIGN = spconfig['design']
 SHAREPOINT_PUBLIC = spconfig['public']
-SHAREPOINT_DOC = spconfig['doc_library']
 SHAREPOINT_LIST = spconfig['list']
 AUTH = HttpNtlmAuth(USERNAME,PASSWORD)
 HEADERS = {'Accept': 'application/json;odata=verbose',"content-type": "application/json;odata=verbose"}
 
-def getToken():
+def get_token():
         contextinfo_api = f"{SHAREPOINT_URL}/_api/contextinfo"
-        response = requests.post(contextinfo_api,auth=AUTH,headers=HEADERS)
+        response = requests.post(contextinfo_api, auth=AUTH, headers=HEADERS)
         response = json.loads(response.text)
         digest_value = response['d']['GetContextWebInformation']['FormDigestValue']
         return digest_value
@@ -35,27 +34,27 @@ def get_subscriber_id(chat):
         value = response["d"]["results"]
         return value[0]["Id"]
 
-def new_subscriber(phone, chat):
+def add_subscriber(phone, chat):
         list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('{SHAREPOINT_LIST}')/items"
         data = {
               '__metadata':  {'type': 'SP.Data.TestListItem' },
               'TelePhone': phone,
               'TeleChat': chat
               }
-        new_headers = HEADERS.copy()
-        new_headers['X-RequestDigest'] = getToken()
+        add_headers = HEADERS.copy()
+        add_headers['X-RequestDigest'] = get_token()
         try:
-               with requests.post(list_url, verify=False, auth=AUTH, headers=new_headers, data=json.dumps(data)) as response:
+               with requests.post(list_url, verify=False, auth=AUTH, headers=add_headers, data=json.dumps(data)) as response:
                       response.raise_for_status()
                       return json.loads(response.text)
         except requests.exceptions.RequestException as e:
                 print(f"Error occurred: {e}")
                 return None
         
-def check_phone(chat):
+def check_subscriber(chat):
         list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('{SHAREPOINT_LIST}')/items?$filter=TeleChat eq '{chat}'"
         get_headers = HEADERS.copy()
-        get_headers['X-RequestDigest'] = getToken()
+        get_headers['X-RequestDigest'] = get_token()
         response = requests.get(list_url, verify=False, auth=AUTH, headers=get_headers)
         response = json.loads(response.text)
         value = response["d"]["results"]
@@ -65,21 +64,21 @@ def delete_subscriber(chat):
         id = get_subscriber_id(chat)
         list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('{SHAREPOINT_LIST}')/Items({id})"
         delete_headers = HEADERS.copy()
-        delete_headers['X-Http-Method'] = 'DELETE' #to delete
-        delete_headers['If-Match'] = '*' #to delete
-        delete_headers['X-RequestDigest'] = getToken()
+        delete_headers['X-Http-Method'] = 'DELETE'
+        delete_headers['If-Match'] = '*'
+        delete_headers['X-RequestDigest'] = get_token()
         response = requests.post(list_url, auth=AUTH, headers=delete_headers)
         return response
 
-def update_item(id,phone):
+def update_subscriber(id,phone):
         update_api = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('{SHAREPOINT_LIST}')/Items({id})"
         data = {
               '__metadata':  {'type': 'SP.Data.TestListItem' },
               'Title': phone
               }
         upd_headers = HEADERS.copy()
-        upd_headers['X-Http-Method'] = 'MERGE' #to update
-        upd_headers['If-Match'] = '*' #to update
+        upd_headers['X-Http-Method'] = 'MERGE'
+        upd_headers['If-Match'] = '*'
         response = requests.post(update_api, auth=AUTH, headers=upd_headers, data=json.dumps(data))
         return json.loads(response.text)
 
@@ -92,7 +91,7 @@ def get_last_token():
                 }
         }
         get_headers = HEADERS.copy()
-        get_headers['X-RequestDigest'] = getToken()
+        get_headers['X-RequestDigest'] = get_token()
         response = requests.post(list_url, verify=False, auth=AUTH, headers=get_headers, data=json.dumps(data))
         response_json = json.loads(response.text)
         value = response_json['d']['results']
@@ -114,58 +113,59 @@ def get_changes():
                         }
                 }
                 get_headers = HEADERS.copy()
-                get_headers['X-RequestDigest'] = getToken()
+                get_headers['X-RequestDigest'] = get_token()
                 response = requests.post(list_url, verify=False, auth=AUTH, headers=get_headers, data=json.dumps(data))
                 response_json = json.loads(response.text)
                 results = response_json['d']['results']
                 if results:
                         created_item = results[0]
-                        item_id = created_item['ItemId']
-                        if is_assignedto_subscriber(item_id):
-                                return print("yes")
-                        else:
-                                return print ('no')
+                        task_id = created_item['ItemId']
+                        return task_id
         else:
                 return None
 
-def get_task_assignedto_OrgID(task_id):
-        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('Задачи рабочих процессов (Vitro)')/items({task_id})"
+def get_task_assignedto_OrgID(task_id, chat_data):
+        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('Задачи рабочих процессов (Vitro)')/items({task_id})?$select=VitroWorkflowAssignedTo"
         get_headers = HEADERS.copy()
-        get_headers['X-RequestDigest'] = getToken()
+        get_headers['X-RequestDigest'] = get_token()
         response = requests.get(list_url, verify=False, auth=AUTH, headers=get_headers)
         response = json.loads(response.text)
         value = response["d"]['VitroWorkflowAssignedTo']
         return value
 
-def get_task_assignedto_FizID(task_id):
-        org_id = get_task_assignedto_OrgID(task_id)
-        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('Организационно-штатная структура')/items({org_id})"
+def get_task_assignedto_FizID(task_id, chat_data):
+        org_id = get_task_assignedto_OrgID(task_id, chat_data)
+        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('Организационно-штатная структура')/items({org_id})?$select=VitroOrgPerson"
         get_headers = HEADERS.copy()
-        get_headers['X-RequestDigest'] = getToken()
+        get_headers['X-RequestDigest'] = get_token()
         response = requests.get(list_url, verify=False, auth=AUTH, headers=get_headers)
         response = json.loads(response.text)
         value = response["d"]['VitroOrgPerson']
         return value
 
-def get_task_assignedto_Phone(task_id):
-        fiz_id = get_task_assignedto_FizID(task_id)
-        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('Физические лица')/items({fiz_id})"
+def get_task_assignedto_Phone(task_id, chat_data):
+        fiz_id = get_task_assignedto_FizID(task_id, chat_data)
+        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('Физические лица')/items({fiz_id})?$select=VitroOrgPhone"
         get_headers = HEADERS.copy()
-        get_headers['X-RequestDigest'] = getToken()
+        get_headers['X-RequestDigest'] = get_token()
         response = requests.get(list_url, verify=False, auth=AUTH, headers=get_headers)
         response = json.loads(response.text)
-        value = response["d"]['VitroOrgPhone']  # Проверка наличия значения
+        value = response["d"]['VitroOrgPhone']
         return value if value else None
 
-def is_assignedto_subscriber(task_id):
-        phone = get_task_assignedto_Phone(task_id)
-        if not phone:  # Проверка на пустое значение
+def is_assignedto_subscriber(task_id, chat_data):
+        if task_id is None:
                 return False
-        phone = re.sub('[^A-Za-z0-9]+', '', phone)
-        list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('{SHAREPOINT_LIST}')/items?$filter=TelePhone eq '{phone}'"
-        get_headers = HEADERS.copy()
-        get_headers['X-RequestDigest'] = getToken()
-        response = requests.get(list_url, verify=False, auth=AUTH, headers=get_headers)
-        response = json.loads(response.text)
-        value = response["d"]["results"]
-        return bool(value)
+        phone = get_task_assignedto_Phone(task_id, chat_data)
+        if phone is None:
+                return False
+        else:
+                phone = re.sub('[^A-Za-z0-9]+', '', phone)
+                list_url = f"{SHAREPOINT_URL}/_api/Web/Lists/GetByTitle('{SHAREPOINT_LIST}')/items?$filter=TelePhone eq '{phone}'"
+                get_headers = HEADERS.copy()
+                get_headers['X-RequestDigest'] = get_token()
+                response = requests.get(list_url, verify=False, auth=AUTH, headers=get_headers)
+                response = json.loads(response.text)
+                value = response["d"]["results"]
+                chat_data['TeleChat'] = value[0]["TeleChat"]
+                return bool(value)
